187d44c6c540146d8d196c6ad4fc1a55609e0a82
Cate Palmer <catherinepalmer@gmail.com>
2018-10-15 16:14:17 +1300

    Changed components slightly to reflect minesweeper game

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,17 +1,31 @@
 import styled from 'styled-components'
 
+const beginner = {
+  numRows: 9,
+  numColumns: 9
+}
+
+const intermediate = {
+  numRows: 16,
+  numColumns: 16
+}
+
+const expert = {
+  numRows: 16,
+  numColumns: 30
+}
+
 const Board = styled.div`
   align-self: center;
   display: grid;
   grid-area: board;
   grid-gap: 0;
-  grid-template-areas: 'zero one two' 'three four five' 'six seven eight';
-  grid-template-columns: 20vh 20vh 20vh;
-  grid-template-rows: 20vh 20vh 20vh;
-  height: 60vh;
+  grid-template-columns: ${expert.numColumns * 3.5}vh;
+  grid-template-rows: 9vh ${expert.numRows * 3.5}vh;
+  height: ${9 + expert.numRows * 3.5}vh;
   justify-self: center;
   margin: auto;
-  width: 60vh;
+  width: ${expert.numColumns * 3.5}vh;
 `
 Board.displayName = 'Board'
 
--- src/components/Square/index.js
+++ src/components/Square/index.js
@@ -2,22 +2,27 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledSquare = styled.div`
-  border-color: hsla(0, 0%, 0%, 0.2);
+  border-color: hsla(0, 0%, 48%, 1);
   border-style: solid;
   border-width: 2px;
-  color: gray;
-  font-size: 16vh;
+  color: ${({isTouching}) => ((isTouching === 1) ? 'hsla(82, 56%, 15%, 1)' :
+                              (isTouching === 2) ? 'hsla(92, 89%, 24%, 1)' :
+                              (isTouching === 3) ? 'hsla(354, 76%, 42%, 1)' :
+                              (isTouching === 4) ? 'hsla(77, 44%, 6%, 1)' :
+                              (isTouching === 5) ? 'hsla(18, 95%, 22%, 1)' :
+                              (isTouching === 6) ? 'hsla(104, 83%, 23%, 1)' :
+                              (isTouching === 7) ? 'hsla(60, 86%, 5%, 1)' : 'hsla(60, 82%, 29%, 1)')};
+  font-size: 3vh;
   font-weight: bold;
-  line-height: 20vh;
+  line-height: 3.5vh;
   text-align: center;
-  text-transform: uppercase;
 `
 StyledSquare.displayName = 'StyledSquare'
 
 export default function Square (props) {
   return (
-    <StyledSquare index={props.index} player={props.player}>
-      {props.player}
+    <StyledSquare index={props.index} isHidden={props.isHidden} isMarked={props.isMarked} isMine={props.isMine} isTouching={thisSquare.isTouching}>
+      
     </StyledSquare>
   )
 }
