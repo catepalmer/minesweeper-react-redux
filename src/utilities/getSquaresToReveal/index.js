17631e15d04a46f13dedac367aed05d773a76aaf
Cate Palmer <catherinepalmer@gmail.com>
2018-10-21 14:14:03 +1300

    Refactored getSquaresToReveal function slightly but still not working correctly

--- src/utilities/getSquaresToReveal/index.js
+++ src/utilities/getSquaresToReveal/index.js
@@ -1,4 +1,4 @@
-import { contains, forEach } from 'ramda'
+import { contains, forEach, prepend } from 'ramda'
 import getMinesTouching from '../getMinesTouching'
 import getSquares from '../getSquares'
 
@@ -7,12 +7,13 @@ export default function getSquaresToReveal(index, mines, squaresToReveal = []) {
   const minesTouching = getMinesTouching(index, mines)
   const squares = getSquares(index)
 
-  (isMine || (minesTouching > 0)) ? squaresToReveal.push(index)
-                                  : squaresToReveal.push(index)
-                                    forEach((square => {
-                                      squaresToReveal.push(square)
-                                      getSquaresToReveal(square, mines, squaresToReveal)
-                                    }), squares)
+  if (minesTouching === 0 && isMine === false) {
+    forEach((square => {
+      squaresToReveal.push(square)
+      getSquaresToReveal(square, mines, squaresToReveal)
+      return squaresToReveal
+    }), squares)
+  }
 
-  return squaresToReveal
+  return prepend(index, squaresToReveal)
 }
