83a1ed7d286ba8b9145e44febefcac50271832d9
catepalmer <catherinepalmer@gmail.com>
2018-10-24 16:26:53 +1300

    Wrote new version of getSquaresToReveal function that hopefully will use tail call optimisation to call the function recursively

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,10 +1,12 @@
 import setMines from './setMines'
 import getMinesTouching from './getMinesTouching'
+import getSquaresToReveal from './getSquaresToReveal'
 import getSquaresTouching from './getSquaresTouching'
 import checkIfPlayed from './checkIfPlayed'
 
 export {
          checkIfPlayed,
+         getSquaresToReveal,
          getMinesTouching,
          getSquaresTouching,
          setMines
