ec9393e803f6ad6affb50f5701bffc2f2a8f51aa
catepalmer <catherinepalmer@gmail.com>
2018-10-30 16:40:58 +1300

    Refactored getSquaresToReveal function to use iteration instead of recursion and got it working but very ugly and not DRY

--- src/utilities/getSquaresToReveal/index.js
+++ src/utilities/getSquaresToReveal/index.js
@@ -1,185 +1,139 @@
-import { append, concat, contains, find, forEach, not, uniq } from 'ramda'
-import { isUndefined } from 'ramda-adjunct'
+import { append, concat, contains, forEach, length, not, uniq } from 'ramda'
 import getMinesTouching from '../getMinesTouching'
 import getSquaresTouching from '../getSquaresTouching'
 
-export default function getSquaresToReveal(index, mines, squaresToReveal = [], squaresTouching = []) {
+export default function getSquaresToReveal(index, mines, squaresToReveal = []) {
   const isMine = contains(index, mines)
   const minesTouching = getMinesTouching(index, mines)
-  squaresToReveal = append(index, [...squaresToReveal])
-  let squaresNotRevealed = []
-  
-  if (isMine) return squaresToReveal
-  if (minesTouching === 0) squaresTouching = uniq(concat(getSquaresTouching(index), [...squaresTouching]))
-  
-  forEach((square => (not(contains(square, [squaresToReveal]))) ? squaresNotRevealed = append(square, squaresNotRevealed) : ''), squaresTouching)
-  
-  if (length(squaresNotRevealed) === 0) return squaresToReveal
-  
-  return getSquaresToReveal(squaresNotRevealed[0], mines, squaresToReveal, squaresTouching)
+  squaresToReveal = append(index, squaresToReveal)
+
+  if (isMine || minesTouching > 0) return squaresToReveal
+
+  let squaresTouching = getSquaresTouching(index)
+
+  for (let i = 0; i < squaresTouching.length; i++) {
+    squaresToReveal = append(squaresTouching[i], squaresToReveal)
+    if (getMinesTouching(squaresTouching[i], mines) === 0) {
+      let squaresTouching2 = getSquaresTouching(squaresTouching[i])
+      for (let j = 0; j < squaresTouching2.length; j++) {
+        squaresToReveal = append(squaresTouching2[j], squaresToReveal)
+        if (getMinesTouching(squaresTouching2[j], mines) === 0) {
+          let squaresTouching3 = getSquaresTouching(squaresTouching2[j])
+          for (let k = 0; k < squaresTouching3.length; k++) {
+            squaresToReveal = append(squaresTouching3[k], squaresToReveal)
+            if (getMinesTouching(squaresTouching3[k], mines) === 0) {
+              let squaresTouching4 = getSquaresTouching(squaresTouching3[k])
+              for (let l = 0; l < squaresTouching4.length; l++) {
+                squaresToReveal = append(squaresTouching4[l], squaresToReveal)
+                if (getMinesTouching(squaresTouching4[l], mines) === 0) {
+                  let squaresTouching5 = getSquaresTouching(squaresTouching4[l])
+                  for (let m = 0; m < squaresTouching5.length; m++) {
+                    squaresToReveal = append(squaresTouching5[m], squaresToReveal)
+                    if (getMinesTouching(squaresTouching5[m], mines) === 0) {
+                      let squaresTouching6 = getSquaresTouching(squaresTouching5[m])
+                      for (let n = 0; n < squaresTouching6.length; n++) {
+                        squaresToReveal = append(squaresTouching6[n], squaresToReveal)
+                        if (getMinesTouching(squaresTouching6[n], mines) === 0) {
+                          let squaresTouching7 = getSquaresTouching(squaresTouching6[n])
+                          for (let o = 0; o < squaresTouching7.length; o++) {
+                            squaresToReveal = append(squaresTouching7[o], squaresToReveal)
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return length(uniq(squaresToReveal))
 }
 
 
 
 
 
-// export default function getSquaresToReveal(index, mines, squaresToReveal = [], squaresTouching = []) {
-//   const isMine = contains(index, mines)
-//   const minesTouching = getMinesTouching(index, mines)
-//   squaresToReveal = append(index, [...squaresToReveal])
-
-
-
-//   forEach((sq => (not(contains(sq, [squaresToRevealNew]))) ? newSquare = sq : ''), squaresTouching)
-
-//   return newSquareToReveal
-//   }
-
-//   if (isMine || isUndefined(nextSquareTouching)) return squaresToReveal
-//   if (minesTouching > 0) return getSquaresToReveal(nextSquareTouching, mines, squaresToRevealNew, squaresTouchingNew)
-  
-//   squaresTouchingNew = uniq(concat(getSquaresTouching(index), squaresTouching))
-//   nextSquareTouching = find(not(forEach((x => x), squaresTouchingNew)))(squaresToRevealNew)
-
-//   return getSquaresToReveal(nextSquareTouching, mines, squaresToRevealNew, squaresTouchingNew)
-// }
-
-// Need to pseudocode this out: We pass in the index of the square we're checking and the mines array.
-// We check if the square we've passed in is a mine - if it is, we return squaresToReveal as an empty array
-// (for now - will figure out what to do when a mine is clicked later). We also check how many mines the square
-// that's been passed in is touching - if it's touching more than 0, we just add this square to the squaresToReveal
-// array and then return this without running the getSquaresToReveal function any more times.
-// If it's touching 0 squares though, we want to add all the squares it's touching that aren't already in the
-// squaresToReveal array to the squaresToReveal array. Then
-
-
-
-
-// import { append, concat, contains, find, forEach, not, uniq } from 'ramda'
-// import { isUndefined } from 'ramda-adjunct'
+// import { append, concat, contains, forEach, length, not, uniq } from 'ramda'
 // import getMinesTouching from '../getMinesTouching'
 // import getSquaresTouching from '../getSquaresTouching'
-
-// export default function getSquaresToReveal(index, mines, squaresToReveal = [], squaresTouching = []) {
-//   const isMine = contains(index, mines)
-//   const minesTouching = getMinesTouching(index, mines)
-//   const squaresToRevealNew = append(index, [...squaresToReveal])
-//   let squaresTouchingNew = [...squaresTouching]
-//   let nextSquareTouching = find(not(forEach((x => x), squaresTouchingNew)))(squaresToRevealNew)
-
-//   if (isMine || isUndefined(nextSquareTouching)) return squaresToRevealNew
-//   if (minesTouching > 0) return getSquaresToReveal(nextSquareTouching, mines, squaresToRevealNew, squaresTouchingNew)
-  
-//   squaresTouchingNew = uniq(concat(getSquaresTouching(index), squaresTouching))
-//   nextSquareTouching = find(not(forEach((x => x), squaresTouchingNew)))(squaresToRevealNew)
-
-//   return getSquaresToReveal(nextSquareTouching, mines, squaresToRevealNew, squaresTouchingNew)
-// }
-
-
-
-
-// not(contains((forEach((square => square), squaresTouchingNew), squaresToRevealNew))
-
-//   const nextSquareTouching = find(forEach((square => square), squaresToRevealNew))(squaresTouchingNew)
-
-
-// var xs = [{a: 1}, {a: 2}, {a: 3}];
-// R.find(R.propEq('a', 2))(xs); //=> {a: 2}
-// R.find(R.propEq('a', 4))(xs); //=> undefined
-
-
-
-// import { contains, forEach, append, uniq } from 'ramda'
-// import { isUndefined } from 'ramda-adjunct'
-// import getMinesTouching from '../getMinesTouching'
-// import getSquaresTouching from '../getSquaresTouching'
-
-// export default function getSquaresToReveal(index, mines, squaresToReveal = [], squaresTouching = []) {
-//   const isMine = contains(index, mines)
-//   const minesTouching = getMinesTouching(index, mines)
-//   const squaresTouchingNew = uniq(append(getSquaresTouching(index), squaresTouching))
-//   const squaresToRevealNew = append(index, squaresToReveal)
-
-//   if (isMine || minesTouching > 0) return squaresToRevealNew
-
-//   const nextSquareTouching = find(forEach((square => square), squaresTouchingNew))(squaresToRevealNew)
-
-//   if(isUndefined(nextSquareTouching)) return squaresToRevealNew
-
-//   return getSquaresToReveal(nextSquareTouching, mines, squaresToReveal)
-// }
-
-
-
-  
-//     forEach((square => {
-//       squares.push(square + index)
-//       getSquaresToReveal((square + index), mines, squares)
-//       return squares
-//     }), squaresTouching)
-
-// }
-
-
-// find(forEach((square => square), squaresTouchingNew))(squaresToRevealNew)
-
-
-// var xs = [{a: 1}, {a: 2}, {a: 3}];
-// R.find(R.propEq('a', 2))(xs); //=> {a: 2}
-// R.find(R.propEq('a', 4))(xs); //=> undefined
-
-
-// function getMatchingSquare (squaresTouching, squaresToReveal) {
-//     forEach((x => forEach((y => (x === y) ? x : null), squaresToReveal)), squaresTouching)
-// }
-
-
-
-// function getMatchingSquare (squaresTouching, squaresToReveal) {
-//     forEach((x => forEach((y => (x === y) ? x : null), squaresToReveal)), squaresTouching)
-// }
-
-
-// function getMatchingSquare (squaresTouching, squaresToReveal) {
-//     forEach((square => {
-//         forEach((sq => {
-//             (square === sq) ? square : null
-//         }), squaresToReveal)
-//     }), squaresTouching)
-// }
-
-
-
-// function getMatch (squaresTouching, squaresToReveal) {
-//     for (var i = 0; i < squaresTouching.length; i++) {
-//         for (var j = 0; j < squaresToReveal.length; j++) {
-//             (squaresToReveal[j] === squaresTouching[i]) ? squaresTouching[i] : null
-//             }
-//     }
-// }
-
-
-
-
-
-
-// import { contains, forEach, prepend } from 'ramda'
-// import getMinesTouching from '../getMinesTouching'
-// import getSquares from '../getSquares'
 
 // export default function getSquaresToReveal(index, mines, squaresToReveal = []) {
 //   const isMine = contains(index, mines)
 //   const minesTouching = getMinesTouching(index, mines)
-//   const squares = getSquares(index)
+//   squaresToReveal = append(index, squaresToReveal)
 
-//   if (minesTouching === 0 && isMine === false) {
-//     forEach((square => {
-//       squaresToReveal.push(square + index)
-//       getSquaresToReveal((square + index), mines, squaresToReveal)
-//       return squaresToReveal
-//     }), squares)
+//   if (isMine || minesTouching > 0) return squaresToReveal
+
+//   let squaresTouching = getSquaresTouching(index)
+
+//   for (let i = 0; i < squaresTouching.length; i++) {
+//     squaresToReveal = append(squaresTouching[i], squaresToReveal)
+//     if (getMinesTouching(squaresTouching[i], mines) === 0) {
+//       let squaresTouching2 = getSquaresTouching(squaresTouching[i])
+//       for (let j = 0; j < squaresTouching2.length; j++) {
+//         squaresToReveal = append(squaresTouching2[j], squaresToReveal)
+//         if (getMinesTouching(squaresTouching2[j], mines) === 0) {
+//           let squaresTouching3 = getSquaresTouching(squaresTouching2[j])
+//           for (let k = 0; k < squaresTouching3.length; k++) {
+//             squaresToReveal = append(squaresTouching3[k], squaresToReveal)
+//             if (getMinesTouching(squaresTouching3[k], mines) === 0) {
+//               let squaresTouching4 = getSquaresTouching(squaresTouching3[k])
+//               for (let l = 0; l < squaresTouching4.length; l++) {
+//                 squaresToReveal = append(squaresTouching4[l], squaresToReveal)
+//                 if (getMinesTouching(squaresTouching4[l], mines) === 0) {
+//                   let squaresTouching5 = getSquaresTouching(squaresTouching4[l])
+//                   for (let m = 0; m < squaresTouching5.length; m++) {
+//                     squaresToReveal = append(squaresTouching5[m], squaresToReveal)
+//                     if (getMinesTouching(squaresTouching5[m], mines) === 0) {
+//                       let squaresTouching6 = getSquaresTouching(squaresTouching5[m])
+//                       for (let n = 0; n < squaresTouching6.length; n++) {
+//                         squaresToReveal = append(squaresTouching6[n], squaresToReveal)
+//                         if (getMinesTouching(squaresTouching6[n], mines) === 0) {
+//                           let squaresTouching7 = getSquaresTouching(squaresTouching6[n])
+//                           for (let o = 0; o < squaresTouching7.length; o++) {
+//                             squaresToReveal = append(squaresTouching7[o], squaresToReveal)
+//                           }
+//                         }
+//                       }
+//                     }
+//                   }
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
 //   }
-
-//   return prepend(index, squaresToReveal)
+//   return length(uniq(squaresToReveal))
 // }
+
+
+
+
+
+
+  // function getSquaresToReveal(index, mines, squaresToReveal = [], squaresTouching = []) {
+  //   const isMine = contains(index, mines)
+  //   let minesTouching = getMinesTouching(index, mines)
+  //   squaresToReveal = append(index, [...squaresToReveal])
+  //   let squaresNotRevealed = []
+
+  //   if (isMine) return squaresToReveal
+  //   if (minesTouching === 0) squaresTouching = uniq(concat(getSquaresTouching(index), [...squaresTouching]))
+
+  //   forEach((square => {
+  //     if (not(contains(square, [squaresToReveal]))) {
+  //       squaresNotRevealed = append(square, squaresNotRevealed)
+  //     }
+  //   }), squaresTouching)
+
+  //   if (length(squaresNotRevealed) === 0) return squaresToReveal
+
+  //   return getSquaresToReveal(squaresNotRevealed[0], mines, squaresToReveal, squaresTouching)
+  // }
+
