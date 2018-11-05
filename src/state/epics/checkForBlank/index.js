dc1c3748db3b77e76007b9023f1db69b75488252
catepalmer <catherinepalmer@gmail.com>
2018-11-05 20:51:06 +1300

    Finish checkForBlank epic

--- src/state/epics/checkForBlank/index.js
+++ src/state/epics/checkForBlank/index.js
@@ -1,36 +1,25 @@
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/mergeMap'
 import 'rxjs/add/observable/of'
-import { head, length, union } from 'ramda'
-import { isNonEmptyArray } from 'ramda-adjunct'
+import { filter, forEach, not } from 'ramda'
 
-import { getMoves, SQUARE_CLICKED } from '../..'
+import { getMoves, SQUARE_CLICKED, squareClicked } from '../..'
 import { checkIfPlayed, getMinesTouching, getSquaresTouching } from '../../../utilities'
 
-export default function checkForWinEpic (action$, store) {
+export default function checkForBlankEpic (action$, store) {
   return action$.ofType(SQUARE_CLICKED).mergeMap(({ payload }) => {
     const square = payload.square
-    const moves = getMoves(store.getState())
-    const isPlayed = checkIfPlayed(square, moves)
     const minesTouching = getMinesTouching(square, mines)
-    const squaresTouching = getSquaresTouching(square)
 
     if (minesTouching > 0) {
-      // do nothing - no other squares are revealed if the square that was clicked
-      // on is touching any mines.
+        return Observable.of()
     }
 
+    const moves = getMoves(store.getState())
+    const squaresToReveal = filter((x => not(checkIfPlayed(x, moves)), getSquaresTouching(square)))
 
-    // if (isNonEmptyArray(wins)) {  // found at least one winning pattern!
-    //   // game over! somebody won
-    //   // return a wrapped gameOver action with the winning squares and the player
-    // }
-
-    // if (plays > 8) { // no more squares to play
-    //   // game over! (it's a tie)
-    //   // return a wrapped empty gameOver action to indicate a tie
-    // }
-
-    // do nothing (none of the above conditions met)
+    forEach((x => {
+        return Observable.of(squareClicked(x))
+    }), squaresToReveal)
   })
 }
