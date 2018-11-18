d8c57328b3db643edaa575c9553b3f8146e24226
catepalmer <catherinepalmer@gmail.com>
2018-11-18 16:13:02 +1300

    Added dummy data to allow schema to be tested without going to the db

--- packages/server/schema/schema.js
+++ packages/server/schema/schema.js
@@ -1,5 +1,12 @@
 const graphql = require('graphql')
 const { GraphQLObjectType, GraphQLID, GraphQLInt, GraphQLSchema } = graphql
+const { find } = require('ramda')
+
+// dummy data
+const games = [
+  {id: 1, flags: [11, 19], mines: [11, 19, 50, 56, 58, 66, 70, 76, 77, 80], moves: [0, 1, 9, 10, 36, 37, 38, 39, 27, 28, 29, 30, 20, 21]},
+  {id: 2, flags: [4], mines: [4, 8, 24, 27, 32, 34, 37, 43, 51, 53], moves: [0, 1, 2, 3, 9, 10, 11, 12, 18, 19, 20, 21, 22, 29, 30, 31, 13]}
+]
 
 const GameType = new GraphQLObjectType({
   name: 'Game',
@@ -19,6 +26,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db
+        return find({ id: args.id }, games)
       }
     }
   }
