8a03ed2d2426a1042e5d0495a7d98e6e1a7da4b8
catepalmer <catherinepalmer@gmail.com>
2018-11-19 17:22:58 +1300

    Got resolve function working using ramda find

--- packages/server/schema/schema.js
+++ packages/server/schema/schema.js
@@ -1,12 +1,11 @@
 const graphql = require('graphql')
 const { GraphQLObjectType, GraphQLID, GraphQLInt, GraphQLList, GraphQLSchema, GraphQLString } = graphql
-const ramda = require('ramda')
-const { find } = ramda
+const { find } = require('ramda')
 
 // dummy data
 const games = [
-  {id: 1, flags: [11, 19], mines: [11, 19, 50, 56, 58, 66, 70, 76, 77, 80], moves: [0, 1, 9, 10, 36, 37, 38, 39, 27, 28, 29, 30, 20, 21]},
-  {id: 2, flags: [4], mines: [4, 8, 24, 27, 32, 34, 37, 43, 51, 53], moves: [0, 1, 2, 3, 9, 10, 11, 12, 18, 19, 20, 21, 22, 29, 30, 31, 13]}
+  {id: "1", flags: [11, 19], mines: [11, 19, 50, 56, 58, 66, 70, 76, 77, 80], moves: [0, 1, 9, 10, 36, 37, 38, 39, 27, 28, 29, 30, 20, 21]},
+  {id: "2", flags: [4], mines: [4, 8, 24, 27, 32, 34, 37, 43, 51, 53], moves: [0, 1, 2, 3, 9, 10, 11, 12, 18, 19, 20, 21, 22, 29, 30, 31, 13]}
 ]
 
 const GameType = new GraphQLObjectType({
@@ -26,19 +25,12 @@ const RootQuery = new GraphQLObjectType({
       type: GameType,
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
-        return games.find(game => {
-          console.log(`args.id: ${args.id}, game.id: ${game.id}`)
-          return game.id === args.id
-        })
+        return find(game => game.id === args.id)(games)
         // code to get data from db
       }
     }
   }
 })
-
-
-R.find(R.propEq('a', 4))(xs); //=> undefined
-
 
 
 module.exports = new GraphQLSchema({
